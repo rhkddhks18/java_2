d44bba5ba6d328cfac7829dc3e8fd072f32f7368
깃rhkddhks18 <taengja18@naver.com>
2023-09-20 11:15:25 +0900

    Car

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class Car {
+    int speed;
+    String model;
+    String accel() {
+        return "달립니다";
+    }
+    String fly() {
+        return "날아갑니다";
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int c = a;
-        a = b;
-        b = c;
-        System.out.println("a\n"+a+b);
-        System.out.println(b);
+        Car car1 = new Car();
+        car1.speed = 80;
+        car1.model = "k5";
+        System.out.println(car1.speed);
+        System.out.println(car1.accel());
+        System.out.println(car1.model + "가 최고속력 " + car1.speed + "km로 " + car1.accel());
+
+        Car car2 = new Car();
+        car2.speed = 100;
+        car2.model = "k9";
+        System.out.println(car2.speed);
+        System.out.println(car2.accel());
+        System.out.println(car2.model + "가 최고속력 " + car2.speed + "km로 " + car2.fly());
     }
 }
