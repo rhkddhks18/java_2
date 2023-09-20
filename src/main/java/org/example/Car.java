4b4b2b7f74c0644f4e56e0da6b342c001dc9b5aa
깃rhkddhks18 <taengja18@naver.com>
2023-09-20 11:20:38 +0900

    car1

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,11 +2,7 @@ package org.example;
 
 class Car {
     int speed;
-    String model;
-    String accel() {
-        return "달립니다";
-    }
-    String fly() {
-        return "날아갑니다";
+    static String run(){
+        return "달린다";
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,6 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.speed = 80;
-        car1.model = "k5";
-        System.out.println(car1.speed);
-        System.out.println(car1.accel());
-        System.out.println(car1.model + "가 최고속력 " + car1.speed + "km로 " + car1.accel());
-
-        Car car2 = new Car();
-        car2.speed = 100;
-        car2.model = "k9";
-        System.out.println(car2.speed);
-        System.out.println(car2.accel());
-        System.out.println(car2.model + "가 최고속력 " + car2.speed + "km로 " + car2.fly());
-    }
+        System.out.println(Car.run());
+     }
 }
